25eb9013ca310d6bd4a8b201e6cf42b59f80dbed
Quin Schurman <schurmanq@gmail.com>
2023-04-19 16:30:21 -0400

    Initial Commit

---
+++ modules/bridge/rpx/client.lua
@@ -0,0 +1,34 @@
+local onLogout = ...
+
+RegisterNetEvent('ox:playerLogout', onLogout)
+
+AddStateBagChangeHandler("job", nil, function(bagName, key, value) 
+    if GetPlayerFromStateBagName(bagName) ~= PlayerId() then return end
+	local plyState = LocalPlayer.state
+	local PlayerData = {}
+    PlayerData.groups[plyState.job.name] = plyState.job.rank
+	OnPlayerData("groups", PlayerData.groups)
+end)
+
+AddStateBagChangeHandler("gang", nil, function(bagName, key, value) 
+    if GetPlayerFromStateBagName(bagName) ~= PlayerId() then return end
+	local plyState = LocalPlayer.state
+	local PlayerData = {}
+    PlayerData.groups[plyState.gang.name] = plyState.gang.rank
+	OnPlayerData("groups", PlayerData.groups)
+end)
+
+---@diagnostic disable-next-line: duplicate-set-field
+function client.setPlayerStatus(values)
+	for name, value in pairs(values) do
+
+		local plyState = LocalPlayer.state
+		if name == "hunger" then
+			TriggerServerEvent('RPX:SetHunger', plyState.metadata.hunger + value)
+		elseif name == "thirst" then
+			TriggerServerEvent('RPX:SetThirst', plyState.metadata.thirst + value)
+		elseif name == "stress" then
+			TriggerServerEvent('RPX:SetStress', plyState.metadata.stress + value)
+		end
+	end
+end
